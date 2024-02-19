2eee3f53acd36f94f4316061638c640a81df3088
Rosvaldas <Roskinatorius>
2024-02-19 19:34:15 +0200

    Setting foundation of the project

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import country from "./countries.json" assert {type: "json"};
+// import country from "./countries.json" assert {type: "json"};
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const API_URL = "https://api.open-meteo.com/v1/forecast";
+const WEATHER_URL = "https://api.openweathermap.org/data/3.0/onecall";
+// ?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+const LOCATION_URL = "https://geokeo.com/geocode/v1/search.php";
+// q=empire+state+building&api=YOUR_API_KEY
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
+
+
 app.listen(port, () => {
     console.log(`The server is running on ${port} port.`);
-    console.log(country[1]);
 })
