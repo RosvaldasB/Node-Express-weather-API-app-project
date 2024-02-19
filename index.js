35e77cc2735c794126c68d2488b168c59a4031d9
Rosvaldas <Roskinatorius>
2024-02-19 23:02:30 +0200

    API's are integrated and ready to be templated into html through EJS

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-// import country from "./countries.json" assert {type: "json"};
+import * as dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+dotenv.config();
 
-const WEATHER_URL = "https://api.openweathermap.org/data/3.0/onecall";
-// ?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-const LOCATION_URL = "https://geokeo.com/geocode/v1/search.php";
-// q=empire+state+building&api=YOUR_API_KEY
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+const LOCATION_URL = "http://api.openweathermap.org/geo/1.0/direct";
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
 
-
+app.post("/get-city", async (req, res) => {
+    try{
+        const requestCity = req.body.city;
+        const cityData = await axios.get(LOCATION_URL, {
+            params: {
+                q: req.body.city,
+                limit: 1,
+                appid: process.env.WEATHER_API,
+            }
+        });
+        const weatherReport = await axios.get(WEATHER_URL, {
+            params: {
+                lat: cityData.data[0].lat,
+                lon: cityData.data[0].lon,
+                units: req.body.units,
+                lang: req.body.lang,
+                appid: process.env.WEATHER_API,
+            },
+        })
+        console.log(weatherReport.data);
+        res.render("index.ejs");
+    } catch (error) {
+        console.error("There was an error: ", error.message);
+    }
+})
 
 app.listen(port, () => {
     console.log(`The server is running on ${port} port.`);
