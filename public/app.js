2e1ae878a522172f4f27508b9e5c5521b18fb653
Rosvaldas <Roskinatorius>
2024-03-05 22:53:35 +0200

    Added animation. Needs responsive design to mobile and tablet, comment out code, make documentation

--- public/app.js
+++ public/app.js
@@ -1,35 +1,21 @@
-// function for API wind deg interpretation
+// introduction div animations
 
-// function windDegInterpretator(deg){
-//     let windDirection;
-//     switch(deg){
-//         case deg >= 351 || deg <= 9:
-//             return windDirection = 'N'
-//             break;
-//         case deg >= 10 && deg <= 80:
-//             windDirection = 'NE'
-//             break;
-//         case deg >= 81 && deg <= 100:
-//             windDirection = 'E'
-//             break;
-//         case deg >= 101 && deg <= 170:
-//             windDirection = 'SE'
-//             break;
-//         case deg >= 171 && deg <= 190:
-//             windDirection = 'S'
-//             break;
-//         case deg >= 191 && deg <= 260:
-//             windDirection = 'SW' 
-//             break;
-//         case deg >= 261 && deg <= 280:
-//             windDirection = 'W'
-//             break;
-//         case deg >= 281 && deg <= 350:
-//             windDirection = 'NW'
-//             break
-//         default:
-//             windDirection = 'There is no wind';
-//     }
+let introductionHeader = gsap.from(".introduction", {opacity: 0});
+introductionHeader = gsap.to(".introduction", {opacity: 1, duration: 3, ease: "expo.out"});
 
-//     return windDirection;
-// }
+let quickLoad = gsap.from(".quick-load", {opacity: 0});
+quickLoad = gsap.to(".quick-load", {opacity: 1, duration: 3, ease: "expo.out"});
+
+// weather app div animation
+
+let mainFrame = gsap.from("#main-frame", {opacity: 0, x: -100, y: -100});
+mainFrame = gsap.to("#main-frame", {opacity: 1, x: 0, y: 0, duration: 1});
+
+let infoHeader = gsap.from(".info-header", {opacity: 0});
+infoHeader = gsap.to(".info-header", {opacity: 1, duration: 2, ease: "expo.out"});
+
+let infoBody = gsap.from(".info-body", {opacity: 0, x: -50});
+infoBody = gsap.to(".info-body", {opacity: 1, x: 0, duration: 1});
+
+let futureForecast = gsap.from(".future-forecast-container", {opacity: 0, x: -50});
+futureForecast = gsap.to(".future-forecast-container", {opacity: 1, x: 0, duration: 1});
